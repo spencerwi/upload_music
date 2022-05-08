59784f442837e2d9a9bc50b56dbbe30407753bd5
Spencer Williams <spencerwi@gmail.com>
2022-05-07 20:20:10 -0400

    Uploading Zip files and extracting them is now supported

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
-[ ] Static "hello, please upload a file!" page
-[ ] Allow uploading zip files -- need to understand any security concerns
-[ ] Auth?
-[ ] Allow reading from a config to specify output location
-[ ] Reading audio file metadata
+[ ] Static "hello, please upload a file!" page  
+[x] Allow uploading zip files -- need to understand any security concerns  
+[ ] Auth?  
+[ ] Configuration file to define upload location and directory/filename pattern to follow for each audio file detected in the zip file
+[ ] Build the piece that reads audio file metadata (start with MP3, maybe add Ogg/AAC/FLAC? support)
+[ ] Build the piece that uses audio file metadata and the pattern from the config file to determine output file structure
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![deny(warnings)]
 
+extern crate tree_magic;
+
+use std::fs::File;
+
 use bytes::BufMut;
 use futures::TryStreamExt;
 use warp::hyper::StatusCode;
@@ -21,7 +25,7 @@ async fn main() {
 
     let upload_route = warp::path("upload")
         .and(warp::post())
-        .and(warp::multipart::form().max_length(5_000_000))
+        .and(warp::multipart::form().max_length(100 * 1024 * 1024))
         .and_then(upload);
 
     let routes = index.or(upload_route).recover(handle_rejection);
@@ -30,50 +34,104 @@ async fn main() {
 }
 
 async fn upload(form: FormData) -> Result<impl Reply, Rejection> {
+    let base_upload_dir = std::env::temp_dir().join("music_upload").join("files");
+
     let parts: Vec<Part> = form.try_collect().await.map_err(|e| {
         eprintln!("form error: {}", e);
         warp::reject::reject()
     })?;
 
     for p in parts {
-        if p.name() == "file" {
-            let content_type = p.content_type();
-            let file_ending;
-            match content_type {
-                Some(file_type) => match file_type {
-                    "text/plain" => {
-                        file_ending = "txt";
-                    }
-                    v => {
-                        eprintln!("invalid file type found: {}", v);
-                        return Err(warp::reject::reject());
-                    }
-                },
-                None => {
-                    eprintln!("file type could not be determined");
+        if p.name() != "file" {
+            // We really only care about the file upload right now.
+            // In the future, maybe we'll extract information about the file from
+            //  request headers or something.
+            continue;
+        }
+
+        let content_type = p.content_type();
+        let file_ending;
+        match content_type {
+            Some(file_type) => match file_type {
+                "application/zip" | "application/octet-stream" => {
+                    file_ending = "zip";
+                }
+                v => {
+                    eprintln!("invalid file type found: {}", v);
                     return Err(warp::reject::reject());
                 }
+            },
+            None => {
+                eprintln!("file type could not be determined");
+                return Err(warp::reject::reject());
             }
-
-            let value = p.stream()
-                .try_fold(Vec::new(), |mut vec, data| {
-                    vec.put(data);
-                    async move { Ok(vec) }
-                })
-                .await
-                .map_err(|e| {
-                    eprintln!("reading file error: {}", e);
-                    warp::reject::reject()
-                })?;
-
-            let file_name = format!("./files/{}.{}", Uuid::new_v4().to_string(), file_ending);
-            let _ = tokio::fs::write(&file_name, value).await.map_err(|e| {
-                eprintln!("error writing file: {}", e);
-                warp::reject::reject()
-            });
-
-            println!("created file: {}", file_name);
         }
+
+        let value = p.stream()
+            .try_fold(Vec::new(), |mut vec, data| {
+                vec.put(data);
+                async move { Ok(vec) }
+            })
+            .await
+            .map_err(|e| {
+                eprintln!("reading file error: {}", e);
+                warp::reject::reject()
+            })?;
+
+        let uploaded_file_mimetype = tree_magic::from_u8(&value);
+        if uploaded_file_mimetype != "application/zip" {
+            eprintln!("Non-zipfile upload detected");
+            return Err(warp::reject::reject());
+        }
+        let file_uuid = Uuid::new_v4().to_string();
+        let file_name = base_upload_dir.join(format!("{}.{}", file_uuid, file_ending));
+        match tokio::fs::create_dir_all(&base_upload_dir).await {
+            Ok(_) => {},
+            Err(e) => {
+                eprintln!(
+                    "error creating file upload directory {}: {}",
+                    base_upload_dir.to_string_lossy(),
+                    e
+                );
+            }
+        }
+        match tokio::fs::write(&file_name, value).await {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("error writing file: {}", e);
+                return Err(warp::reject::reject());
+            }
+        };
+
+        println!("Uploaded zip file: {}", file_name.to_string_lossy());
+        println!("Unpacking zip file {} now", file_name.to_string_lossy());
+        let file = File::open(&file_name).unwrap();
+        let archive = zip::ZipArchive::new(file);
+        match archive {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Something is wrong with the uploaded zip file: {}", e);
+                return Err(warp::reject::reject());
+            }
+        }
+        // TOOD: we should validate the contents of the zip file!
+        let target_dir = base_upload_dir.join(format!("extracted-{}", file_uuid));
+        match tokio::fs::create_dir_all(&target_dir).await {
+            Ok(_) => {},
+            Err(e) => {
+                eprintln!("failed to create extract dir {}: {}", target_dir.to_string_lossy(), e);
+                return Err(warp::reject::reject());
+            }
+        };
+
+        match archive.unwrap().extract(&target_dir) {
+            Ok(_) => {},
+            Err(e) => {
+                eprintln!("failed to extract {}: {}", file_name.to_string_lossy(), e);
+                return Err(warp::reject::reject());
+            }
+        }
+        println!("Unpacked {} to {}", file_name.to_string_lossy(), target_dir.to_string_lossy());
     }
 
     Ok("success")
